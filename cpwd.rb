7ad587235e20243d742714ecbeb3c7ccd415a756
songqi <65730629@qq.com>
2021-10-20 16:05:01 +0800

    Version 1.0.0

---
+++ cpwd.rb
@@ -0,0 +1,13 @@
+class CpwdBinary < Formula
+  desc ""
+  homepage "https://github.com/songqii/cpwd"
+  url "https://github.com/songqii/cpwd_binary/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "6d56e911c8a40c906121aba31653bea4a6dfa4e75117d00d7d82cc60747f4389"
+  license ""
+
+
+  def install
+    bin.install 'cpwd'
+  end
+
+end
