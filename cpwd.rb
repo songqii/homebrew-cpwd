db632afcd0309f11f95e5fa9d6d2f5ff99a5f02a
songqi <65730629@qq.com>
2021-10-20 16:15:24 +0800

    update rb

--- cpwd.rb
+++ cpwd.rb
@@ -3,7 +3,8 @@ class CpwdBinary < Formula
   homepage "https://github.com/songqii/cpwd"
   url "https://github.com/songqii/cpwd_binary/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "6d56e911c8a40c906121aba31653bea4a6dfa4e75117d00d7d82cc60747f4389"
-  license ""
+  version "1.0.0"
+  license "MIT"
 
 
   def install
